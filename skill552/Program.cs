bb5c5651ec9161bd2aa16e41dab11a9f5dea2352
eugeniyFH <shadrin_evgeni07@mail.ru>
2024-12-28 06:04:24 +0700

    итоговое задание 5 модуля

--- skill552/Program.cs
+++ skill552/Program.cs
@@ -16,13 +16,98 @@ namespace skill552
             var count = Console.ReadLine();
             if (count == "Да")
                 ShowArray(array, true);
-            
-            
-                
+
+            //int[] array = { 6, 23, 7, 98, 12, 56 };
+            //Console.WriteLine("Напишите слово для эха");
+            //var modif = Console.ReadLine();
+            //int deep = modif.Length;
+            //Console.WriteLine(modif);
+
+            //Echo(modif, deep);
+            //Console.WriteLine("Введите чилсо для возведения вв степень ");
+            //var n = int.Parse(Console.ReadLine());
+            //int count = n;
+            //Console.WriteLine(n * PowerUp(n, ref count));
+            //SortArray( array);
             //var sortedarray = SortArray(array);
         }
-        
-        
+        //static void Echo(string saidword, int deep)
+        //{
+        //    if (saidword.Length > 2) {
+        //        saidword = saidword.Remove(0, 2);
+        //        deep = saidword.Length; }
+        //    Console.BackgroundColor = (ConsoleColor)deep;
+        //    Console.WriteLine("..." + saidword);
+        //    if (deep > 2)
+        //        Echo(saidword, deep);
+
+        //}
+        //static int PowerUp(int N, ref int count)
+        //{
+
+        //    count--;
+        //    if (count == 1)
+        //        return N;
+
+        //    return N * PowerUp(N, ref count);
+        //}
+        //static void SortArray( in int[] array,out int[] sorteddesc,out int[] sortedasc)
+        //{
+        //    sorteddesc = SortArrayDesc(array);
+        //    sortedasc = SortArrayAsc(array);
+        //    foreach(int i in sorteddesc)
+        //        Console.WriteLine(i);
+        //    foreach(int i in sortedasc) Console.WriteLine(i);
+        //}
+        //static int[] SortArrayDesc(int [] sortarray)
+        //{
+        //    int[] sorteddesc = new int [sortarray.Length];
+        //    for (int i = 0; i < sortarray.Length; i++)
+        //        sorteddesc[i] = sortarray[i];
+
+
+        //    int count;
+        //    for (int i = 0; i < sorteddesc.Length; i++)
+        //        for (int j = i + 1; j < sorteddesc.Length; j++)
+        //        {
+        //            if (sorteddesc[i] > sorteddesc[j])
+        //            {
+        //                count = sorteddesc[j];
+        //                sorteddesc[j] = sorteddesc[i];
+        //                sorteddesc[i] = count;
+        //            }
+
+
+
+        //        }
+        //    return sorteddesc;
+
+        //}
+
+        //static int[] SortArrayAsc(int [] sortarray)
+        //{
+        //    int[] sortedasc = new int[sortarray.Length];
+        //    for (int i = 0; i < sortarray.Length; i++)
+        //        sortedasc[i] = sortarray[i];
+        //    int count;
+        //    //for(int k =  0; k < sortedasc.Length; k++)
+        //    for (int i = sortedasc.Length-1; i >= 0; i--)
+        //        for (int j = i-1; j >= 0; j--)
+        //        {
+        //            if (sortedasc[i]  > sortedasc[j])
+        //            {
+        //                count = sortedasc[j];
+        //                sortedasc[j] = sortedasc[i];
+        //                sortedasc[i] = count;
+        //            }
+
+
+
+        //        }
+        //    return sortedasc;
+        //}
+
+
         static int [] GetArrayFromConsole (int num = 10)
         {
             //Console.WriteLine("Введите количество цифр");
--- skilo553/Program.cs
+++ skilo553/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace skilo553
 {
@@ -10,97 +13,139 @@ namespace skilo553
     {
         static void Main(string[] args)
         {
-            int[] array = { 6, 23, 7, 98, 12, 56 };
-
-            Console.WriteLine("Напишите слово для эха");
-            var modif = Console.ReadLine();
-            int deep = modif.Length;
-            Console.WriteLine(modif);
-
-            Echo(modif, deep);
-             Console.WriteLine("Введите чилсо для возведения вв степень ");
-            var n = int.Parse(Console.ReadLine());
-            int count = n;
-            Console.WriteLine(n * PowerUp(n,ref count));
-            //SortArray( array);
-            Console.ReadKey();
             
-        }
-        static void Echo(string saidword, int deep)
-        {
-            if (saidword.Length > 2) { 
-            saidword = saidword.Remove(0, 2);
-            deep = saidword.Length; }
-            Console.BackgroundColor = (ConsoleColor)deep;
-            Console.WriteLine("..."+ saidword );
-            if (deep > 2 )
-                Echo(saidword, deep);
+           Programer();
+            
+            Console.ReadKey();
            
         }
-        static int PowerUp(int N,ref int count)
+       
+        static void /*(string Name, int Age, string LastName, bool Pet,string[] SomePet,  string [] ShowColor)*/ Programer()
+        {
+            //var anketa = (name: Console.ReadLine(), age: int.Parse(Console.ReadLine()));
+            //Console.ReadLine();
+            (string Name, int Age, string LastName, bool Pet, string[] SomePet, string[] ShowColor) user;
+            Console.WriteLine("Введите ваше  имя ,пользователь");
+            user.Name = Console.ReadLine();
+            Console.WriteLine("Введите ваш возраст");
+
+            user.Age = Eror(Console.ReadLine());
+            Console.WriteLine("Введите  вашу фамилию");
+            user.LastName = Console.ReadLine();
+            Console.WriteLine("Есть ли у вас питомцы.Да или Нет");
+            string pet = Console.ReadLine();
+            user.Pet = false;
+            user.SomePet = null;//два начальных значения для полей чтобы избежать путей в ни куда
+            if (pet == "Да")
+            {
+                user.Pet = true;
+                Console.WriteLine("Сколько у вас питомцев?");
+                int SomePet = Eror(Console.ReadLine());
+                user.SomePet = Pet(SomePet);
+            }
+            else if (pet == "Нет")
+                user.Pet = false;
+            else
+            {
+                Console.WriteLine("Введите Да или Нет с большой буквы.Пример=Нет.Да.");
+                pet = Console.ReadLine();
+                if (pet == "Да")
+                { 
+                    user.Pet = true;
+                    Console.WriteLine("Сколько у вас питомцев?");
+                    int SomePet = Eror(Console.ReadLine());
+                    user.SomePet = Pet(SomePet);
+
+                }
+                else if (pet == "Нет")
+                    user.Pet = false;
+            }
+            
+           
+            Console.WriteLine("Сколько У вас любимых цветов?");
+            int some  = Eror(Console.ReadLine());
+            user.ShowColor= ShowColor(some);
+
+
+            //return ( user.Name,  user.Age,  user.LastName,  user.Pet,  user.SomePet,  user.ShowColor) ;
+            OutPut(user.Name, user.Age, user.LastName, user.Pet, user.SomePet, user.ShowColor);
+        }
+        static string [] Pet(int SomePet)
+         {
+            if (SomePet == 0)
+                Console.WriteLine("Питомцы приносят счастье!!!Никогда не поздно завести их");
+            string[] array = new string[SomePet];
+                if (SomePet==1) 
+                Console.WriteLine("Как зовут вашего любичика ");
+                else
+                    Console.WriteLine("Как зовут ваших животных");
+            for (int i = 0; i < SomePet; i++)
+            {
+               array[i] = Console.ReadLine();
+            }
+            return array;
+         }
+        static string [] ShowColor(int color )
+        {
+            if (color == 0)
+            {
+                Console.WriteLine("Как же так у вас не любимых цветов ???Может вы стесняетесь ?Да раслабься ,это останется между нами ");
+                color = int.Parse(Console.ReadLine());
+            }
+            string[] array = new string[color];
+            Console.WriteLine("Введите ваши люмые цвета");
+            for (int i = 0;i < color; i++)
+            {
+                array [i] = Console.ReadLine();
+             
+            }
+
+            return array;
+
+        }
+
+       static int Eror(string number)
+        {
+             int count=0;
+           
+          if (count > 10)
+            {
+             Console.WriteLine("Ошибка.Может что-то не так?");
+              return 0;
+           }
+              
+          if (int.TryParse(number,out int intnum) )
+             return int.Parse(number);
+          else 
+           {
+           Console.WriteLine("Вы ввели неправильно.Введите число от 0 и больше ");
+            number = Console.ReadLine();
+            count++;
+           return Eror(number);
+           }
+               
+                    
+            
+
+        }
+        static void OutPut(string Name, int Age, string LastName,  bool Pet, string [] SomePet, string [] ShowColor)
         {
             
-            count--;
-            if (count == 1)
-                return N;
-             
-            return N * PowerUp(N ,ref count);
+            Console.WriteLine($"Данные пользователя:\t{Name}\t{Age}\t{LastName}");
+            Console.Write("Имеются питомцы:");
+            if (Pet == true) {
+                for(int i = 0; i < SomePet.Length; i++) 
+                    Console.Write(SomePet[i] + " ");  
+            }
+            else Console.Write("Нет");
+            Console.WriteLine();
+            Console.Write("Имеются любимые цвета:");
+            for(int i = 0;i < ShowColor.Length ; i++)
+                Console.Write(ShowColor[i] + " ");
         }
-        //static void SortArray( in int[] array,out int[] sorteddesc,out int[] sortedasc)
-        //{
-        //    sorteddesc = SortArrayDesc(array);
-        //    sortedasc = SortArrayAsc(array);
-        //    foreach(int i in sorteddesc)
-        //        Console.WriteLine(i);
-        //    foreach(int i in sortedasc) Console.WriteLine(i);
-        //}
-        //static int[] SortArrayDesc(int [] sortarray)
-        //{
-        //    int[] sorteddesc = new int [sortarray.Length];
-        //    for (int i = 0; i < sortarray.Length; i++)
-        //        sorteddesc[i] = sortarray[i];
 
 
-        //    int count;
-        //    for (int i = 0; i < sorteddesc.Length; i++)
-        //        for (int j = i + 1; j < sorteddesc.Length; j++)
-        //        {
-        //            if (sorteddesc[i] > sorteddesc[j])
-        //            {
-        //                count = sorteddesc[j];
-        //                sorteddesc[j] = sorteddesc[i];
-        //                sorteddesc[i] = count;
-        //            }
-
-
-
-        //        }
-        //    return sorteddesc;
-
-        //}
-
-        //static int[] SortArrayAsc(int [] sortarray)
-        //{
-        //    int[] sortedasc = new int[sortarray.Length];
-        //    for (int i = 0; i < sortarray.Length; i++)
-        //        sortedasc[i] = sortarray[i];
-        //    int count;
-        //    //for(int k =  0; k < sortedasc.Length; k++)
-        //    for (int i = sortedasc.Length-1; i >= 0; i--)
-        //        for (int j = i-1; j >= 0; j--)
-        //        {
-        //            if (sortedasc[i]  > sortedasc[j])
-        //            {
-        //                count = sortedasc[j];
-        //                sortedasc[j] = sortedasc[i];
-        //                sortedasc[i] = count;
-        //            }
-
-
-
-        //        }
-        //    return sortedasc;
-        //}
     }
+    
 }
  
